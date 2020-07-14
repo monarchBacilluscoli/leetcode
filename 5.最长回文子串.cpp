eef8c5eca63c160035e6d51230c62ac46a8499da
liuyongfeng <531817981@qq.com>
2020-07-14 15:31:33 +0800

    ？中间伸长我会，但是动态规划咋弄？

--- 5.最长回文子串.cpp
+++ 5.最长回文子串.cpp
@@ -4,8 +4,8 @@
  * [5] 最长回文子串
  */
 
-#include <string>
-using namespace std;
+// #include <string>
+// using namespace std;
 
 // @lc code=start
 class Solution
@@ -18,25 +18,44 @@ public:
             return "";
         }
         string max_string = "";
-        for (size_t i = 0; i < s.size(); i++)
+        for (int i = 0; i < s.size(); i++)
         {
+            // from a char
             int current_length = 1;
-            for (size_t j = 0;; j++)
+            for (int j = 1;; j++)
             {
                 if (i + j < s.size() && i - j >= 0 && s[i + j] == s[i - j])
                 {
-                    ++current_length;
+                    current_length += 2;
                 }
                 else
                 {
                     if (current_length > max_string.size())
                     {
-                        max_string = string::
+                        max_string = s.substr(i - j + 1, 2 * (j - 1) + 1);
                     }
+                    break;
+                }
+            }
+            // between two chars
+            current_length = 0;
+            for (int j = 0;; j++)
+            {
+                if (i + 1 + j < s.size() && i - j >= 0 && s[i + 1 + j] == s[i - j])
+                {
+                    current_length += 2;
+                }
+                else
+                {
+                    if (current_length > max_string.size())
+                    {
+                        max_string = s.substr(i - j + 1, 2 * (j - 1) + 2);
+                    }
+                    break;
                 }
             }
         }
-        return max_length;
+        return max_string;
     }
 };
 // @lc code=end
